853ba52061b111aff90a08b40542b48fb167ed61
Sameersunny <sunny.sahotra@gmail.com>
2025-09-07 10:03:54 +0300

    Cors changes

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['POST', 'GET', 'PUT', 'OPTIONS', '*'],
 
-    'allowed_origins' => ['*', 'http://localhost', 'https://localhost'],
+    'allowed_origins' => ['*', 'http://localhost:4200', 'http://127.0.0.1:4200'],
 
     'allowed_origins_patterns' => [],
 
