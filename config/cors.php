c4b58d6914e6abd5911dc584b2318ff54330a0bf
z.mansoor@jusour.qa <z.mansoor@jusour.qa>
2025-11-10 14:50:21 +0300

    configuration modified

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,10 @@ return [
         'http://localhost:4200',
         'http://127.0.0.1:4200',
         'http://192.168.201.3:86',
-        'http://localhost:5000'
+        'http://localhost:5000',
+        'http://localhost:8068',
+        'http://localhost:8067',
+        'http://localhost:8069'
     ],
     'allowed_origins_patterns' => [],
 
