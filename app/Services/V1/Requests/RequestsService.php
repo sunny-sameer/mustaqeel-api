8d8648d47a064ed172e82f395a3943e5da3ed87a
Talha <talhakhalid2424@gmail.com>
2026-01-07 12:49:46 +0300

    resolved the multi level approval error

--- app/Services/V1/Requests/RequestsService.php
+++ app/Services/V1/Requests/RequestsService.php
@@ -420,13 +420,15 @@ class RequestsService extends BaseService
         if ($stageName == 'Application') {
             $request = $this->requestsInterface->show($reqId);
             $data2['userId'] = $request->userId;
-        }
-
-        $user = $this->usersInterface->getUserById($data2['userId']);
-        $getUserRequestStatusExistence = $this->requestsInterface->getUserRequestStatus($reqId,$user);
-        if(!isset($getUserRequestStatusExistence->id) || (isset($getUserRequestStatusExistence->id) && empty($getUserRequestStatusExistence->endDate))){
             $requestStatusData = RequestStatusDTO::fromRequest($data2)->toArray();
             $this->requestsInterface->createRequestStageStatus($data2, $requestStatusData, $this->status);
+        }else{
+            $user = $this->usersInterface->getUserById($data2['userId']);
+            $getUserRequestStatusExistence = $this->requestsInterface->getUserRequestStatus($reqId,$user);
+            if(!isset($getUserRequestStatusExistence->id) || (isset($getUserRequestStatusExistence->id) && empty($getUserRequestStatusExistence->endDate))){
+                $requestStatusData = RequestStatusDTO::fromRequest($data2)->toArray();
+                $this->requestsInterface->createRequestStageStatus($data2, $requestStatusData, $this->status);
+            }
         }
 
         $stageStatus = $this->requestsInterface->getRequestStatus($reqId);
